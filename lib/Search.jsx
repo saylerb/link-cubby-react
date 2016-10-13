55bbbdc35052a207bd4fb960ca15aa58dd19256e
Brian Sayler <sayler.b@gmail.com>
2016-10-13 14:31:21 -0600

    add jsx for the link cards

--- lib/Search.jsx
+++ lib/Search.jsx
@@ -3,10 +3,18 @@ const data = require('../public/data')
 
 const Search = () => (
   <div className='container'>
-    {data.links.map((link) => (
-      <h3>{link.title}</h3>
-    ))}
+    <div className='links'>
+      {data.links.map((link) => (
+        <div className='link-card'>
+          <img src={`${link.img}`} className='link-card-img' />
+          <div className='link-card_text'>
+            <h3 className='link-card-title'>{link.title}</h3>
+            <p className='link-card-description'>{link.read ? 'Read' : 'Unread'}</p>
+            <h3>({link.year})</h3>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 )
-
 module.exports = Search
