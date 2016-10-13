1017109c86ca7ce7d9617cacf919e7f440e56129
Brian Sayler <sayler.b@gmail.com>
2016-10-12 23:37:11 -0600

    add React Router
    - pull out landing into its own file
    - add react router

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,14 +1,15 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const Landing = require('./Landing')
+const ReactRouter = require('react-router')
+const Router = ReactRouter.Router
+const Route = ReactRouter.Route
+const hashHistory = ReactRouter.hashHistory
 
 const App = () => (
-  <div className='app-container'>
-    <div className='home-info'>
-      <h1 className='title'>Link Cubby</h1>
-      <input className='search' type='text' placeholder='Search' />
-      <button className='browse-all'>Browse All Links</button>
-    </div>
-  </div>
+  <Router history={hashHistory}>
+    <Route path='/' component={Landing} />
+  </Router>
 )
 
 ReactDOM.render(<App />, document.getElementById('application'))
