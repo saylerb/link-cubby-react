6e7163cd32b9592eabf066aacfd0113c718fd025
Brian Sayler <sayler.b@gmail.com>
2016-10-12 23:13:49 -0600

    add basic app component

--- lib/index.jsx
+++ lib/index.jsx
@@ -2,14 +2,14 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const MyTitle = require('./MyTitle')
 
-const MyFirstComponent = () => {
-  return (
-    <div>
-      <MyTitle title="Hello, World!" color='rebeccapurple' />
-      <MyTitle title="LOL" color='papayawhip' />
-      <MyTitle title="OMGWTFBBQ" color='#f06d06' />
+const App = () => (
+    <div className='app-container'>
+      <div className='home-info'>
+        <h1 className='title'>Link Cubby</h1>
+          <input className='search' type='text' placeholder='Search' />
+          <button className='browse-all'> or Browse All</button>
+      </div>
     </div>
-  )
-}
+)
 
-ReactDOM.render(<MyFirstComponent />, document.getElementById('application'))
+ReactDOM.render(<App />, document.getElementById('application'))
