087d20dc0010d80cae9f27553ec5fac8d28ed7a9
Brian Sayler <sayler.b@gmail.com>
2016-10-14 10:35:28 -0600

    implement searchTerm input
    - added a constructor to the es6 class to hold state
    - added a handleSearchTermEvent method
      - make sure to bind this to the handleSearchTermEvent in the constructor
        (es6 syntax does not autobind, but React.creatClass helper autobinds)
      - onChange (when React sees keyboard press events) will fire off a hand
      - added onChange property to the input field inside the Search component

--- lib/Search.jsx
+++ lib/Search.jsx
@@ -3,12 +3,31 @@ const data = require('../public/data')
 const LinkCard = require('./LinkCard')
 
 class Search extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      searchTerm: 'this is the default searchTerm'
+    }
+
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+  }
+
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render () {
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>Link Cubby</h1>
-          <input className='search-input' type='text' placeholder='Search' />
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input
+            value={this.state.searchTerm}
+            className='search-input'
+            type='text' placeholder='Search'
+            onChange={this.handleSearchTermEvent}
+          />
         </header>
         <div className='links'>
           {data.links.map((link) => (
