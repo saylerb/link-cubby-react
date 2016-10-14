52ccb15421a1ddd3a55c12770628359fe944b57d
Brian Sayler <sayler.b@gmail.com>
2016-10-13 22:52:59 -0600

    convert Search to stateful component

--- lib/Search.jsx
+++ lib/Search.jsx
@@ -2,18 +2,22 @@ const React = require('react')
 const data = require('../public/data')
 const LinkCard = require('./LinkCard')
 
-const Search = () => (
-  <div className='container'>
-    <header className='header'>
-      <h1 className='brand'>Link Cubby</h1>
-      <input className='search-input' type='text' placeholder='Search' />
-    </header>
-    <div className='links'>
-      {data.links.map((link) => (
-        <LinkCard {...link} key={link.id} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>Link Cubby</h1>
+          <input className='search-input' type='text' placeholder='Search' />
+        </header>
+        <div className='links'>
+          {data.links.map((link) => (
+            <LinkCard {...link} key={link.id} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
