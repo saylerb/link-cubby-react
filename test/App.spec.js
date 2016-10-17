b70e175802e9c3c8f590d184228b5ad494257fff
Brian Sayler <sayler.b@gmail.com>
2016-10-17 11:08:17 -0600

    use enzyme to test Search and LinkCard components
    - Search test
      - pull in React, Search component, and enzyme into the spec file
      - use enzyme's shallow method to test if search component mounted
        without loading all the children (LinkCards)
    - LinkCard test
      - pull in LinkCard component and links json
      - test to see if the length of LinkCard array is equal to the length
        of the entire data

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,20 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../lib/Search')
+const LinkCard = require('../lib/LinkCard')
+const { shallow } = require('enzyme')
+const { links } = require('../public/data')
 
 describe('<Search />', () => {
-  it('should pass', () => {
-    expect(true).to.be.true
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className='brand'>Link Cubby</h1>)).to.be.true
+  })
+
+  it('should render all the links', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(LinkCard).length).to.equal(links.length)
   })
 })
