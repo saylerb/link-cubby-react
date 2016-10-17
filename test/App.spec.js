2e3f5af799817fbfb4c02e875a81598bec764fab
Brian Sayler <sayler.b@gmail.com>
2016-10-17 11:33:48 -0600

    use mount() to interact and test Search filter
    - pull in mount method from enzyme
    - test filter method by changing the input (changing state) and ensuring
      that the correct links were filtered

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,7 +4,7 @@ const { expect } = require('chai')
 const React = require('react')
 const Search = require('../lib/Search')
 const LinkCard = require('../lib/LinkCard')
-const { shallow } = require('enzyme')
+const { shallow, mount } = require('enzyme')
 const { links } = require('../public/data')
 
 describe('<Search />', () => {
@@ -16,5 +16,14 @@ describe('<Search />', () => {
   it('should render all the links', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.find(LinkCard).length).to.equal(links.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'hello'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('hello')
+    expect(wrapper.find('.link-card').length).to.equal(1)
   })
 })
