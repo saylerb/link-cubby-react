6dbb19878d09d3d35eb085044eb7d3e300d7702d
Brian Sayler <sayler.b@gmail.com>
2016-10-20 22:57:12 -0600

    add details route and component

---
+++ lib/Details.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class Details extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+        <h1>LOL HI!</h1>
+      </div>
+    )
+  }
+}
+
+module.exports = Details
--- lib/index.jsx
+++ lib/index.jsx
@@ -4,12 +4,14 @@ const Landing = require('./Landing')
 const Search = require('./Search')
 const {Router, Route, IndexRoute, hashHistory} = require('react-router')
 const Layout = require('./Layout')
+const Details = require('./Details')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={Landing} />
       <Route path='/search' component={Search} />
+      <Route path='/details/:id' component={Details} />
     </Route>
   </Router>
 )
