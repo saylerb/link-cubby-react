3c9c6ae895b25245cc5ef93f65435199a8ec1328
Brian Sayler <sayler.b@gmail.com>
2016-10-26 15:07:49 -0600

    display data in the details component
    - add some jsx to the Details component to display details
    - adjusted CSS

--- lib/Details.jsx
+++ lib/Details.jsx
@@ -3,11 +3,20 @@ const { object } = React.PropTypes
 
 class Details extends React.Component {
   render () {
+    const params = this.props.params || {}
+    const { title, url, img, read } = params
+
     return (
-      <div style={{textAlign: 'left'}} className='container'>
-        <pre><code>
-          {JSON.stringify(this.props.params, null, 4)}
-        </code></pre>
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>Link Cubby</h1>
+        </header>
+        <div className='link-info'>
+          <h1>{title}</h1>
+          <h2>{url}</h2>
+          <img className='link-image' src={img} />
+          <h2>{read}</h2>
+        </div>
       </div>
     )
   }
